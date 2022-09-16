aef0771ec6b8e52033bb54227518c7c153b78e52
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-09-16 02:19:32 -0500

    added prompt

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//made a generate passcode function that was missing//
 function generatePassword(){
   // declared varibles//
   var passwordText = [];
@@ -12,6 +13,21 @@ function generatePassword(){
   var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var number = ['0','1','2','3','4','5','6','7','8','9'];
   var symbol = ['~','!','@','#','$','%','^','&','*','(',')','+'];
+
+  var passwordLength = prompt('Choose a password legnth. Must be no less than 8 and no more than 128 characters.');
+  if (passwordLength< 8 || passwordLength > 128) {
+    alert ("Does not meet character requirement");
+    return;
+  }
+
+  else if (isNaN(passwordLength)) { 
+    alert("Please enter numbers.");
+  }
+  for (var i = 0; i < passwordLength; i++) {
+    console.log(i);
+    passwordText += characters[Math.floor(Math.random() * characters.length)]
+  }
+  
   
 
 
