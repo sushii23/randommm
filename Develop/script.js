726cd67aa367f6cdf3cceaff773ab49b13e71758
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-09-16 02:12:11 -0500

    declared var

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-
+  // declared varibles//
+  var passwordText = [];
+  var characters = [];
+  var lowerCase = ['a','b','c','d','e','f','g','f','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z'];
+  var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var number = ['0','1','2','3','4','5','6','7','8','9'];
+  var symbol = ['~','!','@','#','$','%','^','&','*','(',')','+'];
   
+
+
+
 }
 
 // Write password to the #password input
